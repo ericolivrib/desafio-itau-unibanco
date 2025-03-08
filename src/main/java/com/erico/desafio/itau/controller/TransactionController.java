96db7a55e10166bf60c38223d93ab197c500a8fe
Érico Ribeiro <oliv.ericorib@gmail.com>
2025-03-08 15:18:49 -0300

    feat: documentar controller com swagger

--- src/main/java/com/erico/desafio/itau/controller/TransactionController.java
+++ src/main/java/com/erico/desafio/itau/controller/TransactionController.java
@@ -5,6 +5,8 @@ import java.time.OffsetDateTime;
 import java.util.DoubleSummaryStatistics;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,9 @@ import com.erico.desafio.itau.dto.StatisticsResponse;
 import com.erico.desafio.itau.dto.TransactionRequest;
 import com.erico.desafio.itau.service.TransactionService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -32,6 +37,12 @@ public class TransactionController {
   }
 
   @PostMapping("/transacao")
+  @Operation(summary = "Criar transação", description = "Adiciona uma transação na fila de transações", responses = {
+    @ApiResponse(responseCode = "201", description = "Transação adicionada com sucesso"),
+    @ApiResponse(responseCode = "400", description = "JSON de transação malformado"),
+    @ApiResponse(responseCode = "422", description = "Campos informados não atendem os requisitos de transações"),
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+  })
   public ResponseEntity<Void> createTransaction(@RequestBody @Valid TransactionRequest request, UriComponentsBuilder uriBuilder) {
     if (request.dateTime().isAfter(OffsetDateTime.now())) {
       return ResponseEntity.unprocessableEntity().build();
@@ -43,12 +54,20 @@ public class TransactionController {
   }
 
   @DeleteMapping("/transacao")
+  @Operation(summary = "Deletar transações", description = "Limpa todas as transações da fila de transações", responses = {
+    @ApiResponse(responseCode = "200", description = "Transações deletadas com sucesso"),
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+  })
   public ResponseEntity<Void> deleteTransactions() {
     transactionService.clearTransactions();
     return ResponseEntity.ok().build();
   }
 
   @GetMapping("/estatistica")
+  @Operation(summary = "Estatísticas de transações", description = "Recupera as estatísticas das transações adicionadas em um intervalo de tempo", parameters = @Parameter(name = "intervalo", description = "Últimos segundos em que transações foram adicionadas", required = false, example = "60"), responses = {
+    @ApiResponse(responseCode = "200", description = "Estatísticas recuperadas com sucesso"),
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+  })
   public ResponseEntity<StatisticsResponse> getStatistics(@RequestParam(name = "intervalo", defaultValue = "60", required = false) int interval) {
     DoubleSummaryStatistics statistics = transactionService.getStatistics(interval);
 
