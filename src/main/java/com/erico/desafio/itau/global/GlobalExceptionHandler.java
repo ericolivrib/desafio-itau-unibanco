667997f6d70e86ddb0a58c790e90f7611d20be4c
Érico Ribeiro <oliv.ericorib@gmail.com>
2025-03-08 13:40:54 -0300

    feat: criar exception handler

---
+++ src/main/java/com/erico/desafio/itau/global/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.erico.desafio.itau.global;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Void> handleUnprocessableEntity() {
+    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+  }
+  
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<Void> handleBadRequest() {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+  }
+}
