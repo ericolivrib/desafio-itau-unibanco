2d9d4717dcd0470f973a0233bca9f33219f165d2
Érico <oliv.ericorib@gmail.com>
2025-05-20 18:40:49 -0300

    feat: adicionar OpenAPIConfig

---
+++ src/main/java/com/erico/desafio/itau/configuration/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package com.erico.desafio.itau.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Érico O. Ribeiro",
+                        email = "oliv.ericorib@gmail.com",
+                        url = "https://www.github.com/ericolivrib"
+                ),
+                description = "API de cadastro e estatísticas de transações",
+                title = "Desafio Itaú Unibanco - Érico",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Servidor local",
+                        url = "127.0.0.1:8080"
+                )
+        }
+)
+public class OpenAPIConfig {
+}
