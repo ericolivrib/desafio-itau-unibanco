a7cd6e5e49d41a44aab86f80ac9ef956718944b2
Érico <oliv.ericorib@gmail.com>
2025-06-03 22:38:33 -0300

    test: testar retorno de status 422 ao inserir data em momento futuro

--- src/test/java/com/erico/desafio/itau/transaction/controller/TransactionControllerTest.java
+++ src/test/java/com/erico/desafio/itau/transaction/controller/TransactionControllerTest.java
@@ -1,20 +1,15 @@
 package com.erico.desafio.itau.transaction.controller;
 
-import com.erico.desafio.itau.transaction.TransactionService;
 import com.erico.desafio.itau.transaction.dto.TransactionRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.micrometer.core.instrument.binder.http.HttpRequestTags;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -23,7 +18,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,14 +32,10 @@ class TransactionControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockitoBean
-    TransactionService transactionService;
-
     @Test
     @DisplayName("Dado um DTO de transações deve retornar 201 Created ao criar a transação")
     void givenDto_whenCreateTransaction_thenReturn201Created() throws Exception {
         TransactionRequest request = new TransactionRequest(BigDecimal.valueOf(200.00), OffsetDateTime.now());
-        Mockito.when(transactionService.addTransaction(Mockito.eq(request))).thenReturn(Mockito.any(UUID.class));
 
         String requestBody = objectMapper.writeValueAsString(request);
 
@@ -53,6 +43,22 @@ class TransactionControllerTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.header().exists(HttpHeaders.LOCATION))
+                .andExpect(MockMvcResultMatchers.content().string(""));
+    }
+
+    @Test
+    @DisplayName("Dado um DTO de transações com data e hora futura deve retornar 422 Unprocessable Entity ao criar a transação")
+    void givenDtoWithFutureDateTime_whenCreateTransaction_thenReturn422UnprocessableEntity() throws Exception {
+        OffsetDateTime dateTime = OffsetDateTime.now().plusHours(5);
+        BigDecimal value = BigDecimal.valueOf(200.00);
+        
+        TransactionRequest request = new TransactionRequest(value, dateTime);
+
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/transacao").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity())
                 .andExpect(MockMvcResultMatchers.content().string(""));
     }
 }
